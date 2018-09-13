e9427b1f945c61d8eb6d97a361c4065a6a5d70d5
FridayGO1 <370513009@qq.com>
2018-09-13 22:50:26 +0800

    demo小样

---
+++ src/components/Add.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="col-md-4">
+    <from class="form-horizontal">
+      <div class="form-group">
+        <label for="">用户名：</label>
+        <input type="text" class="form-control" placeholder="用户名" v-model="name"/>
+      </div>
+      <div class="form-group">
+        <label for="">评论内容：</label>
+        <textarea rows="6" class="form-control" placeholder="评论内容" v-model="content"></textarea>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button class="btn btn-default pull-right" @click="add">提交</button>
+        </div>
+      </div>
+    </from>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    addComment: { // 指定了属性名、属性值、必要性
+      type: Function,
+      require: true
+    }
+  },
+  data () {
+    return {
+      name: '',
+      content: ''
+    }
+  },
+  methods: {
+    add () {
+      // 1、检查输入的合法性
+      const name = this.name.trim()
+      const content = this.content.trim()
+      if (!name || !content) {
+        alert('姓名或内容不能为空')
+        return
+      }
+      // 2.根据输入的数据，封装成一个comment对象
+      const comment = {
+        name,
+        content
+      }
+      // 3、添加到comments中
+      this.addComment(comment)
+      // 4、清除输入
+      this.name = ''
+      this.content = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Item.vue
@@ -0,0 +1,53 @@
+<template>
+  <li class="list-group-item">
+    <div class="handle">
+      <a href="javascript:;" @click="deleteItem">删除</a>
+    </div>
+    <p class="user"><span>{{comment.name}}</span><span>说：</span></p>
+    <p class="centence">{{comment.content}}</p>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {// 指定属性名和属性值的类型
+    comment: Object,
+    deteleComment: Function,
+    index: Number
+  },
+  methods: {
+    deleteItem () {
+      const {comment, index} = this
+      if (window.confirm(`确定删除${comment.name}的评论吗？`)) {
+        this.deteleComment(index)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  li{
+    transition: .5s;
+    overflow: hidden;
+  }
+  .handle{
+    width: 40px;
+    border: 1px solid #ccc;
+    background: #fff;
+    position: absolute;
+    right: 10px;
+    top: 1px;
+    text-align: center;
+  }
+  .handle a{
+    display: block;
+  }
+  .list-group-item .centence{
+    padding: 0 50px;
+  }
+  .user{
+    font-size: 22px;
+  }
+</style>
