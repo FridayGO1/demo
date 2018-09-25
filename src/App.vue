2044cd99b64c1aee39d485e961220517aa7c6737
FridayGO1 <370513009@qq.com>
2018-09-25 23:04:41 +0800

    '请求'

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,49 @@
 <template>
-    <div class="todo-container">
-      <div class="todo-wrap">
-          <!--<TodoHeader @addTodo="addTodo"/>--><!--绑定监听-->
-          <TodoHeader ref="header"/>
-          <TodoList :todos="todos"/>
-          <TodoFooter :todos="todos" :deteleCompleteTodos="deteleCompleteTodos" :selectAllTodos="selectAllTodos"/>
-      </div>
-    </div>
+  <div>
+    <div v-if="!repoUrl">loading</div>
+    <div v-else>most star repo is <a :href="repoUrl">{{repoName}}</a></div>
+  </div>
 </template>
 
 <script>
-import PubSub from 'pubsub-js'
-import TodoHeader from './components/TodoHeader'
-import TodoList from './components/TodoList'
-import TodoFooter from './components/TodoFooter'
-import storageUtil from './storageUtil.js'
+import axios from 'axios'
 export default {
-  data () {
-    return {
-      // 从localStorage读取todos
-      todos: storageUtil.readTodos()
-    }
-  },
-  mounted () { // 执行异步代码
-    // 给<TodoHeader/>绑定addTodo事件监听
-    this.$ref.header.$on('addTodo', this.addTodo)
-
-    // 订阅消息
-    PubSub.subscribe('delete', (msg, index) => {
-      this.deleteTodo(index)
-    })
-  },
-  methods: {
-    addTodo (todo) {
-      this.todos.unshift(todo)
-    },
-    deteleTodo (index) {
-      this.todos.splice(index, 1)
-    },
-    deteleCompleteTodos () {
-      this.todos = this.todos.filter(todo => !todo.complete)
-    },
-    selectAllTodos (check) {
-      this.todos.forEach(todo => todo.complete = check)
-    }
-  },
-  watch: {
-    todos: {
-      deep: true, // 深度监视
-      handler: function (value) {
-        // 将todos最新的值的json数据，保存到localStorage
-        //window.localStorage.setItem('todos_key', JSON.stringify(value))
-        storageUtil.saveTodos(value)
+ data() {
+   return {
+     repoUrl: '',
+     repoName: ''
+   }
+ },
+  mounted () {
+   const url = `http://api.github.com/search/repositories?q=v&sort=stars`
+    /*this.$http.get(url).then(
+      response => {
+        // 成功了
+        const result =response.data
+        const mostRepo = result.items[0]
+        this.repoUrl = mostRepo.html_url
+        this.repoName = mostRepo.name
+      },
+      response => {
+        alert('请求失败')
       }
-    }
-  },
-  components: {TodoFooter, TodoList, TodoHeader}
+    )*/
+
+    // 使用axios
+    axios.get(url).then(
+      response => {
+        // 成功了
+        const result =response.data
+        const mostRepo = result.items[0]
+        this.repoUrl = mostRepo.html_url
+        this.repoName = mostRepo.name
+      }).catch(error => {
+        alert('请求失败')
+    })
+  }
 }
 </script>
 
 <style scoped>
-.todo-container{
-  width: 600px;
-  margin: 0 auto;
-}
-  .todo-container .todo-wrap{
-    padding: 10px;
-    border: 1px solid #ddd;
-    border-radius: 5px;
-  }
+
 </style>
